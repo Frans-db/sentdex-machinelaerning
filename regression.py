3dd27a4782056772e78e6c85c44db232ae0a210e
Frans <frans-de-boer@hotmail.com>
2020-01-11 16:33:36 +0100

    Regression Training and Testing - Practical Machine Learning Tutorial with Python p.4

--- regression.py
+++ regression.py
@@ -1,6 +1,10 @@
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing, model_selection
 import pandas as pd
+import numpy as np
 import quandl
 import math
+
 
 # get the dataset
 df = quandl.get('WIKI/GOOGL')
@@ -19,8 +23,22 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 # how many days into the future we want to predict
 forecast_out = int(math.ceil(0.01 * len(df)))
+print(f'Shifting data {forecast_out} days into the future')
 # creating the label (data we want to predict)
 df['label'] = df[forecast_col].shift(-forecast_out) 
 df.dropna(inplace=True)
 
-print(df.head())
+# extracting the features and labels
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+# scaling the data 
+X = preprocessing.scale(X)
+# creating train and test data
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+# create classifier and train it
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+# test classifier
+accuracy = clf.score(X_test, y_test)
+print(f'clf accuracy is {accuracy}')
