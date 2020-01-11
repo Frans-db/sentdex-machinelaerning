761c74cae137e2987445d75f19af5e2802d56935
Frans <frans-de-boer@hotmail.com>
2020-01-11 16:57:31 +0100

    Regression forecasting and predicting - Practical Machine Learning Tutorial with Python p.5

--- regression.py
+++ regression.py
@@ -1,10 +1,15 @@
 from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing, model_selection
+from matplotlib import style
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import datetime
 import quandl
 import math
 
+# matplotlib styling
+style.use('ggplot')
 
 # get the dataset
 df = quandl.get('WIKI/GOOGL')
@@ -26,19 +31,45 @@ forecast_out = int(math.ceil(0.01 * len(df)))
 print(f'Shifting data {forecast_out} days into the future')
 # creating the label (data we want to predict)
 df['label'] = df[forecast_col].shift(-forecast_out) 
-df.dropna(inplace=True)
-
-# extracting the features and labels
+# extracting the features
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 # scaling the data 
 X = preprocessing.scale(X)
+# removing forecast_out elements
+X = X[:-forecast_out]
+# features to predict unknown data
+X_lately = X[-forecast_out:]
+# extracting the labels
+df.dropna(inplace=True)
+y = np.array(df['label'])
 # creating train and test data
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
 # create classifier and train it
-clf = LinearRegression()
+clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 # test classifier
 accuracy = clf.score(X_test, y_test)
 print(f'clf accuracy is {accuracy}')
+
+# predicting unknown data
+forecast_set = clf.predict(X_lately)
+
+# creating data for plotting
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+day = 24 * 60 * 50 # day in seconds
+next_unix = last_unix + day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
