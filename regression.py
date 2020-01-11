b83b78a43aa575228043d5483b5dda55ce7646a5
Frans <frans-de-boer@hotmail.com>
2020-01-11 18:29:48 +0100

    How to program the Best Fit Slope - Practical Machine Learning Tutorial with Python p.8

--- regression.py
+++ regression.py
@@ -1,75 +1,15 @@
-from sklearn.linear_model import LinearRegression
-from sklearn import preprocessing, model_selection
-from matplotlib import style
+from statistics import mean
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
-import datetime
-import quandl
-import math
 
-# matplotlib styling
-style.use('ggplot')
+# create data
+xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
+ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
 
-# get the dataset
-df = quandl.get('WIKI/GOOGL')
-# extract useful labels
-df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-# percentage change between low and high
-df['HL_percent'] = ((df['Adj. High'] - df['Adj. Low']) / df['Adj. Low']) * 100
-# percentage change between open and close
-df['PCT_change'] = ((df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open']) * 100
-# extract features
-df = df[['Adj. Close', 'HL_percent', 'PCT_change', 'Adj. Volume']]
+def best_fit_slope(xs, ys):
+    m = (mean(xs) * mean(ys) - mean(xs*ys)) / (mean(xs)**2 - mean(xs**2))
+    b = mean(ys) - m * mean(xs)
+    return m, b
 
-# column we want to forceast
-forecast_col = 'Adj. Close'
-# fill non existant data with -99999
-df.fillna(-99999, inplace=True)
-# how many days into the future we want to predict
-forecast_out = int(math.ceil(0.01 * len(df)))
-print(f'Shifting data {forecast_out} days into the future')
-# creating the label (data we want to predict)
-df['label'] = df[forecast_col].shift(-forecast_out) 
-# extracting the features
-X = np.array(df.drop(['label'], 1))
-# scaling the data 
-X = preprocessing.scale(X)
-# removing forecast_out elements
-X = X[:-forecast_out]
-# features to predict unknown data
-X_lately = X[-forecast_out:]
-# extracting the labels
-df.dropna(inplace=True)
-y = np.array(df['label'])
-# creating train and test data
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-
-# create classifier and train it
-clf = LinearRegression(n_jobs=-1)
-clf.fit(X_train, y_train)
-# test classifier
-accuracy = clf.score(X_test, y_test)
-print(f'clf accuracy is {accuracy}')
-
-# predicting unknown data
-forecast_set = clf.predict(X_lately)
-
-# creating data for plotting
-df['Forecast'] = np.nan
-last_date = df.iloc[-1].name
-last_unix = last_date.timestamp()
-day = 24 * 60 * 50 # day in seconds
-next_unix = last_unix + day
-
-for i in forecast_set:
-    next_date = datetime.datetime.fromtimestamp(next_unix)
-    next_unix += day
-    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
-
-df['Adj. Close'].plot()
-df['Forecast'].plot()
-plt.legend(loc=4)
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.show()
+m, b = best_fit_slope(xs, ys)
+print(m, b)
---
+++ regression_libraries.py
@@ -0,0 +1,75 @@
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing, model_selection
+from matplotlib import style
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import datetime
+import quandl
+import math
+
+# matplotlib styling
+style.use('ggplot')
+
+# get the dataset
+df = quandl.get('WIKI/GOOGL')
+# extract useful labels
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+# percentage change between low and high
+df['HL_percent'] = ((df['Adj. High'] - df['Adj. Low']) / df['Adj. Low']) * 100
+# percentage change between open and close
+df['PCT_change'] = ((df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open']) * 100
+# extract features
+df = df[['Adj. Close', 'HL_percent', 'PCT_change', 'Adj. Volume']]
+
+# column we want to forceast
+forecast_col = 'Adj. Close'
+# fill non existant data with -99999
+df.fillna(-99999, inplace=True)
+# how many days into the future we want to predict
+forecast_out = int(math.ceil(0.01 * len(df)))
+print(f'Shifting data {forecast_out} days into the future')
+# creating the label (data we want to predict)
+df['label'] = df[forecast_col].shift(-forecast_out) 
+# extracting the features
+X = np.array(df.drop(['label'], 1))
+# scaling the data 
+X = preprocessing.scale(X)
+# removing forecast_out elements
+X = X[:-forecast_out]
+# features to predict unknown data
+X_lately = X[-forecast_out:]
+# extracting the labels
+df.dropna(inplace=True)
+y = np.array(df['label'])
+# creating train and test data
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+# create classifier and train it
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+# test classifier
+accuracy = clf.score(X_test, y_test)
+print(f'clf accuracy is {accuracy}')
+
+# predicting unknown data
+forecast_set = clf.predict(X_lately)
+
+# creating data for plotting
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+day = 24 * 60 * 50 # day in seconds
+next_unix = last_unix + day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
